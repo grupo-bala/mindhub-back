a9913e859e584ebeaf126164b6289ca5263c37bc
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-09-21 18:38:37 -0300

    feat: integrate with auth module and update functions signatures to new user primary key

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,50 +1,62 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable, forwardRef } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "./entities/user.entity";
 import { Repository } from "typeorm";
 import { GenericError } from "src/util/error";
+import { AuthService } from "src/auth/auth.service";
 
 export type UserError =
-    | "DUPLICATE_EMAIL"
-    | "DUPLICATE_USERNAME";
+    | "DUPLICATE EMAIL"
+    | "DUPLICATE USERNAME"
+    | "USER DOESNT EXIST";
 
 @Injectable()
 export class UserService {
     constructor(
         @InjectRepository(User)
         private userRepository: Repository<User>,
+        @Inject(forwardRef(() => AuthService))
+        private authService: AuthService,
     ) { }
 
-    async create(createUserDto: CreateUserDto) {
-        if (await this.userRepository.findOneBy({ email: createUserDto.email })) {
-            throw new GenericError<UserError>("DUPLICATE_EMAIL");
-        } else if (await this.userRepository.findOneBy({ username: createUserDto.username })) {
-            throw new GenericError<UserError>("DUPLICATE_USERNAME");
+    async create({ username, password, email }: CreateUserDto) {
+        if (await this.userRepository.findOneBy({ email: email })) {
+            throw new GenericError<UserError>("DUPLICATE EMAIL");
+        } else if (await this.userRepository.findOneBy({ username: username })) {
+            throw new GenericError<UserError>("DUPLICATE USERNAME");
         }
 
-        this.userRepository.save({
-            hashPassword: createUserDto.password,
-            xp: 0,
-            currentBadge: 0,
-            ...createUserDto
+        const { hash, jwt } = await this.authService.signUp(username, password);
+
+        await this.userRepository.save({
+            username,
+            email,
+            hashPassword: hash,
         });
+
+        return jwt;
     }
 
     async findAll() {
         return this.userRepository.find();
     }
 
-    async findOne(id: number) {
-        return this.userRepository.findOneBy({ id });
+    async findOne(username: string) {
+        const user = await this.userRepository.findOneBy({ username });
+        if (user) {
+            return user;
+        } else {
+            throw new GenericError<UserError>("USER DOESNT EXIST");
+        }
     }
 
-    async update(id: number, updateUserDto: UpdateUserDto) {
-        return (await this.userRepository.update(id, updateUserDto))!.affected! > 0;
+    async update(username: string, updateUserDto: UpdateUserDto) {
+        return (await this.userRepository.update(username, updateUserDto))!.affected! > 0;
     }
 
-    async remove(id: number) {
-        return (await this.userRepository.delete(id))!.affected! > 0;
+    async remove(username: number) {
+        return (await this.userRepository.delete(username))!.affected! > 0;
     }
 }
