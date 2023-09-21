51297a293b184bca3978287acaab7b3e9d439b20
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-09-21 00:15:52 -0300

    feat: init user module

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  hashPassword: string;
+
+  @Column()
+  xp: number;
+
+  @Column()
+  currentBadge: number;
+}
