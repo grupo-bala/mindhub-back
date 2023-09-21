40e592db1a935a1cd2ac9340e751e9a0077627f6
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-09-21 00:13:57 -0300

    chore: init swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import "reflect-metadata";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Mindhub')
+    .setDescription('The Mindhub API description')
+    .setVersion('1.0')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
