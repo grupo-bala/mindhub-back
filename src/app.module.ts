5daf8b7ad25cf0d08ca5ec3d7a43d2da33dced2a
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-09-21 00:14:32 -0300

    chore: init database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "postgres",
+      database: "mindhub",
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
+
 export class AppModule {}
