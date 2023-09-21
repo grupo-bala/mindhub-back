96f6fd1bb8a346917e0cad4f712ff4cc54d59cbe
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-09-21 18:36:50 -0300

    refactor: add env vars in db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,31 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UserModule } from "./user/user.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot({
-            type: "postgres",
-            host: "localhost",
-            port: 5432,
-            username: "postgres",
-            password: "postgres",
-            database: "mindhub",
-            synchronize: true,
-            autoLoadEntities: true,
+        ConfigModule.forRoot({
+            isGlobal: true,
+            cache: true,
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                type: "postgres",
+                host: configService.get("DB_HOST"),
+                port: configService.get("DB_PORT"),
+                username: configService.get("DB_USERNAME"),
+                password: configService.get("DB_PASSWORD"),
+                database: configService.get("DB_NAME"),
+                synchronize: true,
+                autoLoadEntities: true,
+            }),
         }),
         UserModule,
+        AuthModule,
     ],
     controllers: [],
     providers: [],
