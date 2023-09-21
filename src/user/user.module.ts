ccb396e4732ba35d2856d54aa4a245eaab819658
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-09-21 18:39:24 -0300

    chore: import auth module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,18 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { UserController } from "./user.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./entities/user.entity";
+import { AuthModule } from "src/auth/auth.module";
+import { AuthService } from "src/auth/auth.service";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
+    imports: [
+        forwardRef(() => AuthModule),
+        TypeOrmModule.forFeature([User])
+    ],
     controllers: [UserController],
-    providers: [UserService],
+    providers: [AuthService, UserService],
 })
 
 export class UserModule { }
