5a2a6984ab82507193b48a8789aa5ccb801c4358
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-09-21 18:40:37 -0300

    test: add placeholder test

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -5,14 +5,18 @@ import { UserService } from "./user.service";
 import { getRepositoryToken } from "@nestjs/typeorm";
 import { User } from "./entities/user.entity";
 import { Repository } from "typeorm";
+import { AuthService } from "src/auth/auth.service";
 
 type Mock = Partial<Repository<User>>;
 
 describe("UserController", () => {
     let controller: UserController;
     const mockRepository: Mock = {};
+    let mockAuth: Partial<AuthService> = {};
 
     beforeEach(async () => {
+        mockAuth = {};
+
         const module: TestingModule = await Test.createTestingModule({
             controllers: [UserController],
             providers: [
@@ -20,6 +24,10 @@ describe("UserController", () => {
                 {
                     provide: getRepositoryToken(User),
                     useValue: mockRepository,
+                },
+                {
+                    provide: AuthService,
+                    useValue: mockAuth,
                 }
             ],
         }).compile();
