340ed78ca22ac08b7ed98d0bc00bad2c5b7a0c2a
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-09-21 02:11:55 -0300

    test: update file to vitest

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,14 +1,27 @@
+import { describe, it, expect, beforeEach, vi } from "vitest";
 import { Test, TestingModule } from "@nestjs/testing";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { User } from "./entities/user.entity";
+import { Repository } from "typeorm";
+
+type Mock = Partial<Repository<User>>;
 
 describe("UserController", () => {
     let controller: UserController;
+    let mockRepository: Mock = {};
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [UserController],
-            providers: [UserService],
+            providers: [
+                UserService,
+                {
+                    provide: getRepositoryToken(User),
+                    useValue: mockRepository,
+                }
+            ],
         }).compile();
 
         controller = module.get<UserController>(UserController);
