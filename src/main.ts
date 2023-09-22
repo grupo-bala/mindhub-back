9ea393255da9848a23424619e3d6fcf213daa73c
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-09-22 16:43:19 -0300

    chore: add bearer authentication to swagger

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
         .setTitle("Mindhub")
         .setDescription("The Mindhub API description")
         .setVersion("1.0")
+        .addBearerAuth()
         .build();
   
     const document = SwaggerModule.createDocument(app, config);
