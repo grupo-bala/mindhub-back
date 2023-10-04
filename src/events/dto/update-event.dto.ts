e920853e30357ab677a6553ddcc8a76927bd47fd
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-10-04 16:50:55 -0300

    fix: fix lint issue

--- src/events/dto/update-event.dto.ts
+++ src/events/dto/update-event.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateEventDto } from './create-event.dto';
+import { PartialType } from "@nestjs/swagger";
+import { CreateEventDto } from "./create-event.dto";
 
 export class UpdateEventDto extends PartialType(CreateEventDto) {}
