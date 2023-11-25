cfc580def06c51c9836b493986f0a9f3382b539d
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-11-25 20:29:44 -0300

    chore: add default entities for test server

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,15 @@ import { MaterialModule } from "./material/material.module";
 import { EventsModule } from "./events/events.module";
 import { PostModule } from "./post/post.module";
 import { BadgeModule } from "./badge/badge.module";
+import { DataSource } from "typeorm";
+import { Badge } from "./badge/entities/badge.entity";
+import { User } from "./user/entities/user.entity";
+import { Expertise } from "./expertise/entities/expertise.entity";
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
-            cache: true,
         }),
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
@@ -41,4 +44,42 @@ import { BadgeModule } from "./badge/badge.module";
     providers: [],
 })
 
-export class AppModule {}
+export class AppModule {
+    constructor(
+        private dataSource: DataSource,
+        private config: ConfigService,
+    ) {
+        if (!this.config.get("IS_TEST_ENV")) {
+            return;
+        }
+
+        this.saveDefaultEntities();
+    }
+
+    private async saveDefaultEntities() {
+        const badges = this.dataSource.getRepository(Badge);
+        const users = this.dataSource.getRepository(User);
+        const expertises = this.dataSource.getRepository(Expertise);
+
+        const expertise = new Expertise();
+        expertise.title = "Matemática";
+        
+        const badge = new Badge();
+        badge.title = "Aprendiz";
+
+        const user = new User();
+        user.name = "teste";
+        user.email = "teste";
+        user.username = "teste";
+        user.hashPassword = "$2b$10$Jnw26Q9ceCviEcMchzAqJu1hg1APTbjZQ4Me821ZMa5OV01e7kNQ6",
+        user.xp = 0;
+        user.badges = [badge];
+        user.currentBadge = badge;
+        user.expertises = [expertise];
+        user.post = [];
+
+        await badges.save(badge);
+        await expertises.save(expertise);
+        await users.save(user);
+    }
+}
