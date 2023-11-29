b5aa3637f5c3dc63fe29b5d680622539ad69f52a
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-11-29 01:44:21 -0300

    feat: add some services imports dependencies in ask module file

--- src/ask/ask.module.ts
+++ src/ask/ask.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AskService } from './ask.service';
-import { AskController } from './ask.controller';
+import { Module } from "@nestjs/common";
+import { AskService } from "./ask.service";
+import { AskController } from "./ask.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Score } from "src/score/entities/score.entity";
+import { Ask } from "./entities/ask.entity";
+import { ScoreModule } from "src/score/score.module";
+import { JwtService } from "@nestjs/jwt";
+import { ScoreService } from "src/score/score.service";
 
 @Module({
-  controllers: [AskController],
-  providers: [AskService],
+    imports: [
+        TypeOrmModule.forFeature([Ask, Score]),
+        ScoreModule,
+    ],
+    controllers: [AskController],
+    providers: [JwtService, ScoreService,AskService],
 })
 export class AskModule {}
