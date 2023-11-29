585a277cd0b11787737abf38cd4c5a2ec51b1dca
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-11-29 01:39:04 -0300

    feat: configurate ask entity to extend post entity with many to one relation with expertise entity

--- src/ask/entities/ask.entity.ts
+++ src/ask/entities/ask.entity.ts
@@ -1,1 +1,9 @@
-export class Ask {}
+import { Expertise } from "src/expertise/entities/expertise.entity";
+import { Post } from "src/post/entities/post.entity";
+import { Entity, ManyToOne } from "typeorm";
+
+@Entity()
+export class Ask extends Post {
+    @ManyToOne(() => Expertise, expertise => expertise.title)
+        expertise: Expertise;
+}
