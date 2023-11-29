770d33f2629223f483d1ad1d5d5906b90f06f94f
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-11-29 20:02:55 -0300

    feat: add default profile picture for users

--- src/static/static.controller.ts
+++ src/static/static.controller.ts
@@ -1,9 +1,10 @@
 import * as fsSync from "fs";
 import * as fs from "fs/promises";
 import * as path from "path";
-import { Controller, Get, Param, Post, Req, Res, UseGuards } from "@nestjs/common";
+import { Controller, Get, HttpStatus, Param, Post, Req, Res, UseGuards } from "@nestjs/common";
 import { Request, Response } from "express";
 import { AuthGuard } from "src/auth/auth.guard";
+import { Readable } from "stream";
 
 @Controller("static")
 export class StaticController {
@@ -26,29 +27,43 @@ export class StaticController {
     }
 
     @Get(":type/:id")
-    @UseGuards(AuthGuard)
     async getFile(
         @Param("type") fileType: string,
-        @Param("id") id: number,
+        @Param("id") id: string,
         @Res() res: Response,
     ) {
-        const file = fsSync.createReadStream(
-            path.join(process.cwd(), "static", fileType, id.toString())
-        );
+        try {
+            return await this.openFile(fileType, id, res);
+        } catch (_) {
+            if (fileType === "user") {
+                return await this.openFile(fileType, "_default", res);
+            }
 
-        file.pipe(res);
+            res.status(HttpStatus.NOT_FOUND);
+            res.send();
+        }
     }
 
     @Post(":type/:id")
     @UseGuards(AuthGuard)
     async sendFile(
         @Param("type") fileType: string,
-        @Param("id") id: number,
+        @Param("id") id: string,
         @Req() req: Request,
     ) {
         await fs.writeFile(
-            path.join(process.cwd(), "static", fileType, id.toString()),
+            path.join(process.cwd(), "static", fileType, id.toString() + ".jpg"),
             req.body,
         );
+    }
+
+    private async openFile(fileType: string, id: string, res: Response) {
+        const file = await fs.readFile(
+            path.join(process.cwd(), "static", fileType, id.toString() + ".jpg")
+        );
+
+        res.contentType("jpg");
+
+        Readable.from(file).pipe(res);
     }
 }
