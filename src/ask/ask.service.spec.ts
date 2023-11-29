a2daebc334b61a3a237f9b47b966f34bd797b122
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-11-29 01:43:02 -0300

    test: set ask service test class

--- src/ask/ask.service.spec.ts
+++ src/ask/ask.service.spec.ts
@@ -1,18 +1,48 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AskService } from './ask.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { AskService } from "./ask.service";
+import { Repository } from "typeorm";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Ask } from "./entities/ask.entity";
+import { ScoreService } from "src/score/score.service";
+import { describe, beforeEach, it, expect } from "vitest";
+import { CreateMaterialDto } from "src/material/dto/create-material.dto";
 
-describe('AskService', () => {
-  let service: AskService;
+describe("AskService", () => {
+    let service: AskService;
+    let mockRepository: Partial<Repository<Ask>> = {};
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AskService],
-    }).compile();
+    beforeEach(async () => {
+        mockRepository = {};
 
-    service = module.get<AskService>(AskService);
-  });
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                AskService,
+                {
+                    provide: getRepositoryToken(Ask),
+                    useValue: mockRepository
+                },
+                {
+                    provide: ScoreService,
+                    useValue: {
+                        getPostScore: async () => 0,
+                        getUserScoreOnPost: async () => 0,
+                    }
+                },
+            ],
+        }).compile();
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+        service = module.get<AskService>(AskService);
+    });
+
+    it("should save correct ask", () => {
+        const ask = new CreateMaterialDto();
+
+        mockRepository.save = async () => [];
+        mockRepository.findOne = async () => {
+            return new Ask();
+        };
+
+        expect(() => service.create(ask, "teste"))
+            .not.toThrow();
+    });
 });
