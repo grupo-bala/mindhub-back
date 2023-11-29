0b7d5231fd2ca4ea22b3705ef0e63c7441801591
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-11-29 01:42:09 -0300

    feat: add create, findOne, find, findAll, remove and update methods in ask service file

--- src/ask/ask.service.ts
+++ src/ask/ask.service.ts
@@ -1,26 +1,145 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAskDto } from './dto/create-ask.dto';
-import { UpdateAskDto } from './dto/update-ask.dto';
+import { Injectable } from "@nestjs/common";
+import { CreateAskDto } from "./dto/create-ask.dto";
+import { UpdateAskDto } from "./dto/update-ask.dto";
+import { Ask } from "./entities/ask.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { ScoreService } from "src/score/score.service";
+import { ExpertiseException } from "src/expertise/expertise.service";
+import { Expertise } from "src/expertise/entities/expertise.entity";
+
+type AskError =
+    | "ASK DOESNT EXIST";
+
+export class AskException extends Error {
+    name: AskError;
+
+    constructor(name: AskError) {
+        super();
+        this.name = name;
+    }
+}
 
 @Injectable()
 export class AskService {
-  create(createAskDto: CreateAskDto) {
-    return 'This action adds a new ask';
-  }
+    constructor(
+        @InjectRepository(Ask)
+        private askRespository: Repository<Ask>,
+        private scoreService: ScoreService,
+    ) { }
 
-  findAll() {
-    return `This action returns all ask`;
-  }
+    async create(
+        { title, content, expertise, postDate} : CreateAskDto,
+        username: string
+    ): Promise<Ask> {
+        try {
+            const { id } = await this.askRespository.save({
+                user: { username },
+                title,
+                content,
+                postDate,
+                expertise: {
+                    title: expertise,
+                    users: [],
+                },
+            });
 
-  findOne(id: number) {
-    return `This action returns a #${id} ask`;
-  }
+            return this.findOne(id, username);
+        } catch (error) {
+            throw new ExpertiseException("EXPERTISE DOESNT EXIST");
+        }
+    }
 
-  update(id: number, updateAskDto: UpdateAskDto) {
-    return `This action updates a #${id} ask`;
-  }
+    async findAll(username: string) {
+        const asks = await this.askRespository.find({
+            relations: {
+                expertise: true,
+                user: {
+                    badges: true,
+                    currentBadge: true,
+                    expertises: true,
+                }
+            }
+        });
 
-  remove(id: number) {
-    return `This action removes a #${id} ask`;
-  }
+        return Promise.all(
+            asks.map(async ask => ({
+                ...ask,
+                score: await this.scoreService.getPostScore(ask.id),
+                userScore: await this.scoreService.getUserScoreOnPost(ask.id, username)
+            }))
+        );
+    }
+
+    async find(usernameTarget: string, usernameViewer: string) {
+        const asks = await this.askRespository.find({
+            where: {
+                user: {
+                    username: usernameTarget,
+                }
+            },
+            relations: {
+                expertise: true,
+                user: {
+                    badges: true,
+                    currentBadge: true,
+                    expertises: true,
+                },
+            },
+        });
+
+        return Promise.all(
+            asks.map(async ask => ({
+                ...ask,
+                score: await this.scoreService.getPostScore(ask.id),
+                userScore: await this.scoreService.getUserScoreOnPost(ask.id, usernameViewer)
+            }))
+        );
+    }
+
+    async findOne(id: number, username: string) {
+        const ask = await this.askRespository.findOne({
+            where: {
+                id
+            },
+            relations: {
+                expertise: true,
+                user : {
+                    badges: true,
+                    currentBadge: true,
+                    expertises: true,
+                }
+            }
+        });
+
+        if (ask) {
+            return {
+                ...ask,
+                score: await this.scoreService.getPostScore(id),
+                userScore: await this.scoreService.getUserScoreOnPost(id, username)
+            };
+        } else {
+            throw new AskException("ASK DOESNT EXIST");
+        }
+    }
+
+    async update(id: number, username: string, updateAskDto: UpdateAskDto) {
+        const expertiseEntity = new Expertise();
+        expertiseEntity.title = updateAskDto.expertise ?? "";
+
+        const parsedDto = {
+            ...updateAskDto,
+            expertise: expertiseEntity,
+        };
+
+        return (
+            await this.askRespository.update({ id, user: { username } }, parsedDto)
+        )!.affected! > 0;
+    }
+
+    async remove(id: number, username: string) {
+        return (
+            await this.askRespository.delete({ id, user: { username } })
+        )!.affected! > 0;
+    }
 }
