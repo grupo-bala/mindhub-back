5badce1fd514744630a341148598daa860f05b29
Antônio Hugo Ribeiro Pereira Lobo <hugorplobo@gmail.com>
2023-11-30 03:18:38 -0300

    chore: update test module

--- src/test/test.module.ts
+++ src/test/test.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { InjectRepository, TypeOrmModule } from "@nestjs/typeorm";
+import { Ask } from "src/ask/entities/ask.entity";
 import { Badge } from "src/badge/entities/badge.entity";
 import { Event } from "src/events/entities/event.entity";
 import { Expertise } from "src/expertise/entities/expertise.entity";
@@ -14,6 +15,7 @@ import { Repository } from "typeorm";
             User,
             Material,
             Event,
+            Ask,
             Badge,
             Expertise
         ]),
@@ -29,9 +31,11 @@ export class TestModule {
         @InjectRepository(Expertise)
         private expertiseRepository: Repository<Expertise>,
         @InjectRepository(Material)
-        private materialRepository: Repository<Material>,
+        private material1Repository: Repository<Material>,
         @InjectRepository(Event)
-        private eventRepository: Repository<Event>
+        private eventRepository: Repository<Event>,
+        @InjectRepository(Ask)
+        private askRepository: Repository<Ask>,
     ) {
         if (this.config.get("IS_TEST_ENV") === "true") {
             this.saveDefaultEntities();
@@ -39,10 +43,15 @@ export class TestModule {
     }
 
     private async saveDefaultEntities() {
-        const expertise = new Expertise();
-        expertise.title = "Matemática";
-        expertise.users = [];
-        await this.expertiseRepository.save(expertise);
+        const expertise1 = new Expertise();
+        expertise1.title = "Matemática";
+        expertise1.users = [];
+
+        const expertise2 = new Expertise();
+        expertise2.title = "Português";
+        expertise2.users = [];
+
+        await this.expertiseRepository.save([expertise1, expertise2]);
 
         const badge = new Badge();
         badge.title = "Aprendiz";
@@ -57,20 +66,28 @@ export class TestModule {
         user.xp = 0;
         user.badges = [badge];
         user.currentBadge = badge;
-        user.expertises = [expertise];
+        user.expertises = [expertise1];
         user.post = [];
         await this.userRepository.save(user);
 
-        const material = new Material();
-        material.title = "Material teste";
-        material.content = "Teste";
-        material.expertise = expertise;
-        material.postDate = "0";
-        material.user = user;
-        await this.materialRepository.save(material);
+        const material1 = new Material();
+        material1.title = "Material matemática";
+        material1.content = "Teste";
+        material1.expertise = expertise1;
+        material1.postDate = "0";
+        material1.user = user;
+
+        const material2 = new Material();
+        material2.title = "Material português";
+        material2.content = "Teste";
+        material2.expertise = expertise2;
+        material2.postDate = "0";
+        material2.user = user;
+
+        await this.material1Repository.save([material1, material2]);
 
         const event = new Event();
-        event.title = "Event teste";
+        event.title = "Evento teste";
         event.content = "Teste";
         event.date = "0";
         event.postDate = "0";
@@ -79,5 +96,21 @@ export class TestModule {
         event.localName = "Local teste";
         event.user = user;
         await this.eventRepository.save(event);
+
+        const ask1 = new Ask();
+        ask1.title = "Pergunta matemática";
+        ask1.content = "Teste";
+        ask1.expertise = expertise1;
+        ask1.postDate = "0";
+        ask1.user = user;
+
+        const ask2 = new Ask();
+        ask2.title = "Pergunta português";
+        ask2.content = "Teste";
+        ask2.expertise = expertise2;
+        ask2.postDate = "0";
+        ask2.user = user;
+
+        await this.askRepository.save([ask1, ask2]);
     }
 }
